fdba35697054259ae8e6833cb19f4c1365d7c37d
YeiderCabeza <ycabeza5@misena.edu.co>
2023-02-07 00:53:48 -0500

    mod 3

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,68 @@
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 
 function App() {
- 
-function Pantalla(props){
+
+  const [valor1, setValor1]=useState("");
+  const [valor2, setvalor2]=useState("");
+  const [op, setOp]=useState("");
+
+  const Operador = (i) => {
+    if (op == "") {
+      setvalor2(parseFloat(valor1));
+      setOp(i.target.value);
+      setValor1("");
+    } else {
+      setOp(i.target.value);
+    }
+  };
+
+  const Resultado =()=>{
+    if(op == "+"){
+      setValor1(parseFloat(valor1) + valor2);
+    }
+    if(op == "-"){
+      setValor1(parseFloat(valor2) - valor1);
+    }
+    if(op == "/"){
+      setValor1(parseFloat(valor2) / valor1);
+    }
+    if(op == "*"){
+      setValor1(parseFloat(valor1) * valor2);
+    }
     
+  }
+
+  const Pantalla =()=>{
+    return(
+      <input type="text" value={valor1} />
+    )
+  }
+
+  function Botones(props){
+    return(
+      <button onClick={() => setValor1(valor1 + (props.num))}>{props.num}</button>
+    )
+  } 
+
+  function BtnOpera(props){
+    return(
+      <button class="btn btn-danger" onClick={Operador} value={props.num}>{props.num}</button>
+    )
+  } 
+
+  function Igual(props){
+    return(
+      <button class="btn btn-danger" onClick={Resultado} value={props.num}>{props.num}</button>
+    )
+  } 
+
+
   return (
     <>
-        <input type="number" name="pantalla" id="pantalla" />
-        <input type="number" name="pantalla" id="pantalla2" /> <br />
-    </>
-    
-  )
-}
-const Boton=()=>{boton(num);}
-
-function Botones(props){
-  return ( <button type="button" class="btn btn-primary" onClick={Boton}>{props.num}</button> )
-}
-
-
-
-
-  return (
-    <>
-        <Pantalla/>
+        <Pantalla/><br />
         <Botones num="1"/>
         <Botones num="2"/>
         <Botones num="3"/>
@@ -37,6 +74,11 @@ function Botones(props){
         <Botones num="9"/>
         <Botones num="0"/>
         <Botones num="="/>
+        <BtnOpera num="*"/>
+        <BtnOpera num="/"/>
+        <BtnOpera num="+"/>
+        <BtnOpera num="-"/>
+        <Igual num="="/>
       
     
     </>

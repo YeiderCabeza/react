5a59590a534627923a784694637c5a84ce35cb49
YeiderCabeza <ycabeza5@misena.edu.co>
2023-02-07 14:21:44 -0500

    mod 3

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,10 +7,10 @@ function App() {
 
   const [valor1, setValor1]=useState("");
   const [valor2, setvalor2]=useState("");
-  const [op, setOp]=useState("");
+  const [opera, setOp]=useState("");
 
   const Operador = (i) => {
-    if (op == "") {
+    if (opera == "") {
       setvalor2(parseFloat(valor1));
       setOp(i.target.value);
       setValor1("");
@@ -20,17 +20,31 @@ function App() {
   };
 
   const Resultado =()=>{
-    if(op == "+"){
+    if(opera == "+"){
       setValor1(parseFloat(valor1) + valor2);
+      setOp("");
+      setValor2("");
+     
     }
-    if(op == "-"){
+    if(opera == "-"){
       setValor1(parseFloat(valor2) - valor1);
+      setOp("");
+      setValor2("");
     }
-    if(op == "/"){
+    if(opera == "/"){
       setValor1(parseFloat(valor2) / valor1);
+      setOp("");
+      setValor2("");
     }
-    if(op == "*"){
+    if(opera == "*"){
       setValor1(parseFloat(valor1) * valor2);
+      setOp("");
+      setValor2("");
+    }
+    if(opera == "%"){
+      setValor1(parseFloat(valor1) * valor2/100);
+      setOp("");
+      setValor2("");
     }
     
   }
@@ -43,7 +57,7 @@ function App() {
 
   function Botones(props){
     return(
-      <button onClick={() => setValor1(valor1 + (props.num))}>{props.num}</button>
+      <button  class="btn btn-dark" onClick={() => setValor1(valor1 + (props.num))}>{props.num}</button>
     )
   } 
 
@@ -59,10 +73,22 @@ function App() {
     )
   } 
 
+  const Eliminar =(props)=>{
+    return(
+      setValor1("")
+    )
+  }
+
+  function Borrar(props){
+    return(
+      <button class="btn btn-danger" onClick={Eliminar} value={props.num}>{props.num}</button>
+    )
+  } 
+
 
   return (
     <>
-        <Pantalla/><br />
+        <Pantalla/><br /> <br />
         <Botones num="1"/>
         <Botones num="2"/>
         <Botones num="3"/>
@@ -72,13 +98,15 @@ function App() {
         <Botones num="7"/>
         <Botones num="8"/>
         <Botones num="9"/>
-        <Botones num="0"/>
-        <Botones num="="/>
+        <Botones num="0"/><br />
+  
+        <BtnOpera num="%"/>
         <BtnOpera num="*"/>
         <BtnOpera num="/"/>
         <BtnOpera num="+"/>
         <BtnOpera num="-"/>
         <Igual num="="/>
+        <Borrar num="⌫"/>
       
     
     </>
